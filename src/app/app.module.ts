05a35151c2fd7c99c18ca5720c4bc7495e5e05f9
Virginia <virginia_hol@hotmail.com>
2021-05-01 15:38:48 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AfficheFilmComponent } from './affiche-film/affiche-film.component';
+import { AfficheSerieComponent } from './affiche-serie/affiche-serie.component';
+import { HomeComponent } from './home/home.component';
+import { DetailFilmComponent } from './detail-film/detail-film.component';
+import { DetailSerieComponent } from './detail-serie/detail-serie.component';
+import { SearchComponent } from './search/search.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AfficheFilmComponent,
+    AfficheSerieComponent,
+    HomeComponent,
+    DetailFilmComponent,
+    DetailSerieComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
